5c82b09ae5398093cb30b946d45a5fac65656d72
Evan <evanslin@gmail.com>
2019-07-08 01:53:32 +0800

    fix rand seed

--- rand.go
+++ rand.go
@@ -1,9 +1,17 @@
 package main
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 const letterBytes = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+//RandStringBytes :Random string by input byte length
 func RandStringBytes(n int) string {
 	b := make([]byte, n)
 	for i := range b {
